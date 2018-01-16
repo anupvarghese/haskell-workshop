20ce7db153702400c98d550c97034195c1eefc7b
Anoop Varghese <anupvarghese@gmail.com>
2018-01-17 09:33:42 +1100

    first

---
+++ hello.hs
@@ -0,0 +1,14 @@
+x = 3
+
+-- Normal function
+f :: Integer -> Integer
+f i = i + 10
+
+--  Lambda Expression
+ff :: Integer -> Integer
+ff = \i -> i + 10
+
+
+-- Another lambda
+fff :: Integer -> Integer -> Integer
+fff = \a b -> (a +b) * 2
