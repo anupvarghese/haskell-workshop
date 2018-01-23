f7560a738fff3e6e3308dc8f8237ac9d1c4ab613
Anoop Varghese <anupvarghese@gmail.com>
2018-01-23 23:24:21 +1100

    finish first exercise

--- sample.hs
+++ sample.hs
@@ -40,3 +40,44 @@ sumEveryTwo :: [Integer] -> [Integer]
 sumEveryTwo [] = []
 sumEveryTwo (x:[]) = [x]
 sumEveryTwo (x:(y:z)) = (x + y) : sumEveryTwo z
+
+-- Homework
+{-
+Credit card validation
+Double the value of every second digit beginning from the right.
+That is, the last digit is unchanged; the second-to-last digit is doubled;
+the third-to-last digit is unchanged; and so on. For example,
+[1,3,8,6] becomes [2,3,16,6].
+• Add the digits of the doubled values and the undoubled digits
+from the original number. For example, [2,3,16,6] becomes
+2+3+1+6+6 = 18.
+Calculate the remainder when the sum is divided by 10. For the
+above example, the remainder would be 8.
+If the result equals 0, then the number is valid
+-}
+
+toDigits :: Integer -> [Integer]
+toDigits n
+  | (n <= 0) = []
+  | otherwise = (n `mod` 10) : (toDigits (n `div` 10))
+
+reverseList :: [Integer] -> [Integer]
+
+reverseList [] = []
+reverseList (h:t) = reverseList t ++ [h]
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n = reverseList (toDigits n)
+
+doubleEveryOther :: [Integer] -> [Integer]
+
+doubleEveryOther [] = []
+doubleEveryOther (x:[]) = [x]
+doubleEveryOther (x:(y:z)) = x : (2 * y) : (doubleEveryOther z)
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (h:t) = (if h > 0 then (h `div` 10) + (h `mod` 10) else h) + sumDigits t
+
+validate :: Integer -> Bool
+validate number = if (sumDigits (doubleEveryOther (toDigits number)) `mod` 10 == 0) then True else False
